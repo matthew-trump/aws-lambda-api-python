cd00101daed11d136a78628848df435020704812
Matthew Trump <matthew.trump@gmail.com>
2018-08-27 09:01:50 -0700

    Update get_quiz_score.py

--- get_quiz_score.py
+++ get_quiz_score.py
@@ -1,4 +1,13 @@
 import boto3;
 
 def handler(event, context):
-  pass
+  userID = event["queryStringParameters"]["userId"]
+  sessionID = event["queryStringParameters"]["sessionId"]
+  
+  numberCorrect = 9
+  numberTotal   = 10
+ 
+  return {
+        "statusCode": '200',
+        "body": 'Your score on the quiz (user='+str(userId)+',sessionId='+str(sessionId)+') was '+ str(numberCorrect) + ' out of ' + str(numberTotal)
+  }
