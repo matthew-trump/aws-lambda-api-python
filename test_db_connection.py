0eb411ec17be2b3223a251c28384f50033fed338
Matthew Trump <matthew.trump@gmail.com>
2018-09-06 09:58:18 -0700

    Update test_db_connection.py

--- test_db_connection.py
+++ test_db_connection.py
@@ -1,16 +1,25 @@
 import boto3;
 import mysql.connector;
 import config;
+import string;
+import random;
 
 def handler(event, context):
-    all_labels = ['alpha','beta','gamma','delta']
+    all_labels = ['cow','animal','beautiful']
     csv_labels = ", ".join(all_labels)
-    key        = "KEY"
+    key        = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+    description = "a mighty cow"
+    
     print("Attempting database connection...")
     
     conn = mysql.connector.connect(user=config.DATABASE_USER, password=config.DATABASE_PASSWORD,
                                    host=config.DATABASE_HOST,
                                    database=config.DATABASE_DB_NAME)
+    
+    
+    conn.cursor().execute("INSERT INTO photo (object_key, description, labels, created_datetime) VALUES (%s,%s,%s,now())",
+                              (key,description,csv_labels))
+    
     cursor = conn.cursor(dictionary=True)
     cursor.execute("""SELECT object_key, description, labels, created_datetime
                       FROM photo""")
