3226624130001216daf47e8e1bf265e7c94d0a14
Matthew Trump <matthew.trump@gmail.com>
2018-09-06 07:46:23 -0700

    Update test_db_connection.py

--- test_db_connection.py
+++ test_db_connection.py
@@ -3,8 +3,22 @@ import mysql.connector;
 import config;
 
 def handler(event, context):
- 
-  return {
+    all_labels = ['alpha','beta','gamma','delta]
+    csv_labels = ", ".join(all_labels)
+    print("Detect_labels finished. Key: [%s], Labels: [%s]" % (key, csv_labels))
+
+    conn = mysql.connector.connect(user=config.DATABASE_USER, password=config.DATABASE_PASSWORD,
+                                   host=config.DATABASE_HOST,
+                                   database=config.DATABASE_DB_NAME)
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""SELECT object_key, description, labels, created_datetime
+                      FROM photo""", (key,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+    #return True
+    return {
         "statusCode": '200',
-        "body": 'It works'
-  }
+        "body": 'It works--connected'
+    }
